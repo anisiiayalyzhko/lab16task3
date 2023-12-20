0bff6e2a7e1387b47ab0da5e8e9f6d2f2bcba346
Anisiia Yalyzhko <anisiya.sumy.ua@gmail.com>
2023-12-21 00:23:40 +0200

    Добавьте файлы проекта.

---
+++ lab16task3/lab16task3.cpp
@@ -0,0 +1,28 @@
+﻿// lab16task3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>  
+#include <cmath> 
+
+int main() {
+	int n;
+	printf("Enter n (size of a matrix NxN):");
+	scanf_s("%d", &n);
+	int** array = new int* [n];
+	for (int i = 0; i < n; i++) {
+		array[i] = new int[n];
+		printf("Enter row with index %d:\n", i);
+		for (int j = 0; j < n; j++) {
+			scanf_s("%d", &array[i][j]);
+		}
+	}
+	int maximalDiagonalNumber = INT_MIN;
+	for (int i = 0; i < n; i++) {
+		if (array[i][i] > maximalDiagonalNumber) {
+			maximalDiagonalNumber = array[i][i];
+		}
+	}
+	printf("Number = %d", maximalDiagonalNumber);
+	return 0;
+}
+
